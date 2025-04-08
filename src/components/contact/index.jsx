c93756c2bb87a762da9fbf8eaf8317fe4fda7c8b
GabrielPlus <gabrielpius0311@gmail.com>
2025-04-08 11:39:27 +0300

    contact page

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -29,8 +29,8 @@ const Contact = () => {
                 </div>
                 <h3>Email Address</h3>
                 <p>
-                  info@webmail.com <br />
-                  jobs@webexample.com
+                  hajimukhtarproperties@gmail.com <br />
+                
                 </p>
               </div>
             </div>
@@ -41,7 +41,7 @@ const Contact = () => {
                 </div>
                 <h3>Phone Number</h3>
                 <p>
-                  +0123-456789 <br /> +987-6543210
+                  +254 720 655 444<br /> +254 734 655 444
                 </p>
               </div>
             </div>
@@ -52,8 +52,7 @@ const Contact = () => {
                 </div>
                 <h3>Office Address</h3>
                 <p>
-                  18/A, New Born Town Hall <br />
-                  New York, US
+                 **
                 </p>
               </div>
             </div>
@@ -62,100 +61,7 @@ const Contact = () => {
       </div>
       {/* <!-- CONTACT ADDRESS AREA END --> */}
 
-      {/* <!-- CONTACT MESSAGE AREA START --> */}
-      <div className="ltn__contact-message-area mb-120 mb--100">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <div className="ltn__form-box contact-form-box box-shadow white-bg">
-                <h4 className="title-2">Get A Quote</h4>
-                <form id="contact-form" action="#" method="post">
-                  <div className="row">
-                    <div className="col-md-6">
-                      <div className="input-item input-item-name ltn__custom-icon">
-                        <input
-                          type="text"
-                          name="name"
-                          placeholder="Enter your name"
-                        />
-                        <span className="inline-icon">
-                          <FaUserAlt />
-                        </span>
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="input-item input-item-email ltn__custom-icon">
-                        <input
-                          type="email"
-                          name="email"
-                          placeholder="Enter email address"
-                        />
-                        <span className="inline-icon">
-                          <FaEnvelope />
-                        </span>
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="input-item input-item input-item-email ltn__custom-icon">
-                        <Form.Select className="nice-select">
-                          <option>Select Service Type</option>
-                          <option>Property Management </option>
-                          <option>Mortgage Service </option>
-                          <option>Consulting Service</option>
-                          <option>Home Buying</option>
-                          <option>Home selling</option>
-                          <option>Escrow Services</option>
-                        </Form.Select>
-                        <span className="inline-icon">
-                          <FaArrowDown />
-                        </span>
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="input-item input-item-phone ltn__custom-icon">
-                        <input
-                          type="text"
-                          name="phone"
-                          placeholder="Enter phone number"
-                        />
-                        <span className="inline-icon">
-                          <FaPhoneAlt />
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="input-item input-item-textarea ltn__custom-icon">
-                    <textarea
-                      name="message"
-                      placeholder="Enter message"
-                    ></textarea>
-                    <span className="inline-icon">
-                      <FaPencilAlt />
-                    </span>
-                  </div>
-                  <p>
-                    <label className="input-info-save mb-0">
-                      <input type="checkbox" name="agree" /> Save my name,
-                      email, and website in this browser for the next time I
-                      comment.
-                    </label>
-                  </p>
-                  <div className="btn-wrapper mt-0">
-                    <button
-                      className="btn theme-btn-1 btn-effect-1 text-uppercase"
-                      type="submit"
-                    >
-                      get a free service
-                    </button>
-                  </div>
-                  <p className="form-messege mb-0 mt-20"></p>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* <!-- CONTACT MESSAGE AREA END --> */}
+     
 
       {/* <!-- GOOGLE MAP AREA START --> */}
       <div className="google-map mb-120">
